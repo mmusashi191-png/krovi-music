cacdfcb42a348dd0a1d3cdee6ccebe4e8770c83c
mmusashi191-png <mmusashi191@gmail.com>
2026-09-24 19:02:42 +0530

    chore: lint source instead of generated Android assets

--- eslint.config.js
+++ eslint.config.js
@@ -5,16 +5,24 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores([
+    'dist/**',
+    'android/**',
+    '.sync-backup-*/**',
+    '**/*.before-*',
+  ]),
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['src/**/*.{js,jsx}', 'server.cjs'],
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
     languageOptions: {
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
   },
