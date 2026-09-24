60225cfbbbe80c9b699cc91233d3125c589b75f9
mmusashi191-png <mmusashi191@gmail.com>
2026-09-24 22:40:10 +0530

    refactor: move startup gate out of entry module

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,14 @@
-import { StrictMode, useEffect, useState } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import StartupScreen from './components/StartupScreen.jsx'
-
-function StartupGate() {
-  const [visible, setVisible] = useState(true)
-  const [exiting, setExiting] = useState(false)
-
-  useEffect(() => {
-    const exitTimer = window.setTimeout(() => setExiting(true), 920)
-    const removeTimer = window.setTimeout(() => setVisible(false), 1320)
-
-    return () => {
-      window.clearTimeout(exitTimer)
-      window.clearTimeout(removeTimer)
-    }
-  }, [])
-
-  return (
-    <>
-      <App />
-      {visible && <StartupScreen exiting={exiting} />}
-    </>
-  )
-}
+import StartupGate from './components/StartupGate.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <StartupGate />
+    <StartupGate>
+      <App />
+    </StartupGate>
   </StrictMode>,
 )
 
